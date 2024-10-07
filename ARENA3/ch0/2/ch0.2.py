b781151eb7f1723ad0453d0101f1da700f58ca8f
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-10-07 22:29:51 +0200

    validation

--- ARENA3/ch0/2/ch0.2.py
+++ ARENA3/ch0/2/ch0.2.py
@@ -74,11 +74,9 @@ class Linear(nn.Module):
         x: shape (*, in_features)
         Return: shape (*, out_features)
         """
-        print(self.weight.shape, x.shape)
-        if len(x.shape > 1):
+        if len(x.shape) > 1:
             out = einops.einsum(self.weight, x, "i j, b j -> b i")
-        # else:
-        #     out = einops.einsum(self.weight, x, "i j, b j -> b i")
+
         if self.bias is not None:
             out += self.bias
         return out
@@ -157,4 +155,104 @@ class SimpleMLP(nn.Module):
 
 tests.test_mlp_module(SimpleMLP)
 tests.test_mlp_forward(SimpleMLP)
+# %%
+
+# Part 2: Training neural networks
+
+MNIST_TRANSFORM = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+def get_mnist(subset: int = 1):
+    '''Returns MNIST training data, sampled by the frequency given in `subset`.'''
+    mnist_trainset = datasets.MNIST(root="./data", train=True, download=True, transform=MNIST_TRANSFORM)
+    mnist_testset = datasets.MNIST(root="./data", train=False, download=True, transform=MNIST_TRANSFORM)
+
+    if subset > 1:
+        mnist_trainset = Subset(mnist_trainset, indices=range(0, len(mnist_trainset), subset))
+        mnist_testset = Subset(mnist_testset, indices=range(0, len(mnist_testset), subset))
+
+    return mnist_trainset, mnist_testset
+
+
+mnist_trainset, mnist_testset = get_mnist()
+mnist_trainloader = DataLoader(mnist_trainset, batch_size=64, shuffle=True)
+mnist_testloader = DataLoader(mnist_testset, batch_size=64, shuffle=False)
+# %%
+mnist_trainloader
+# %%
+# Training loop: add test accuracy
+@dataclass
+class SimpleMLPTrainingArgs():
+    '''
+    Defining this class implicitly creates an __init__ method, which sets arguments as 
+    given below, e.g. self.batch_size = 64. Any of these arguments can also be overridden
+    when you create an instance, e.g. args = SimpleMLPTrainingArgs(batch_size=128).
+    '''
+    batch_size: int = 64
+    epochs: int = 30
+    learning_rate: float = 1e-3
+    subset: int = 10
+
+
+def train(args: SimpleMLPTrainingArgs):
+    '''
+    Trains the model, using training parameters from the `args` object.
+    '''
+    model = SimpleMLP().to(device)
+
+    mnist_trainset, mnist_testset = get_mnist(subset=args.subset)
+    mnist_trainloader = DataLoader(mnist_trainset, batch_size=args.batch_size, shuffle=True)
+    mnist_testloader = DataLoader(mnist_testset, batch_size=args.batch_size, shuffle=False)
+    
+    optimizer = t.optim.Adam(model.parameters(), lr=args.learning_rate)
+    loss_list = []
+    accuracies_list_test = []
+
+    for epoch in tqdm(range(args.epochs)):
+        for imgs, labels in mnist_trainloader:
+            imgs = imgs.to(device)
+            labels = labels.to(device)
+            logits = model(imgs)
+            loss = F.cross_entropy(logits, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            loss_list.append(loss.item())   
+
+        with t.inference_mode():
+            all_preds = []
+            for imgs, labels in mnist_testloader:
+                imgs = imgs.to(device)
+                labels = labels.to(device)
+                logits = model(imgs)
+                
+                all_preds.append((logits.argmax(axis=1) == labels).sum() / labels.shape[0])
+                # accuracy = (logits.argmax(axis=1) == labels) / len(labels)
+                #accuracies_list_test.append(accuracy)
+            accuracies_list_test.append(sum(all_preds) / len(all_preds))
+
+    line(
+        loss_list,
+        yaxis_range=[0, max(loss_list) + 0.1],
+        x=t.linspace(0, args.epochs, len(loss_list)),
+        labels={"x": "Num epochs", "y": "Cross entropy loss"},
+        title="SimpleMLP training on MNIST",
+        width=700,
+    )
+    line(
+        accuracies_list_test,
+        # yaxis_range=[0, max(loss_list) + 0.1],
+        x=t.linspace(0, args.epochs, len(accuracies_list_test)),
+        labels={"x": "Num epochs", "y": "Epoch accuracy"},
+        title="SimpleMLP training on MNIST",
+        width=700,
+    )
+
+
+args = SimpleMLPTrainingArgs()
+train(args)
+# %%
+accuracies_list_test.shape
 # %%
