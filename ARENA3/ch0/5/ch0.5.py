60db58605a8e1ff4bbf4ad14b33cf05cd09baf54
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-11-11 23:13:01 +0100

    started vae

--- ARENA3/ch0/5/ch0.5.py
+++ ARENA3/ch0/5/ch0.5.py
@@ -401,3 +401,86 @@ def visualise_input(
 small_dataset = Subset(get_dataset("MNIST"), indices=range(0, 5000))    
 visualise_input(trainer.model, small_dataset)
 # %%
+# What about variational autoencoders?
+
+class VAE(nn.Module):
+    def __init__(self, latent_dim_size: int, hidden_dim_size: int):
+        super().__init__()
+        # Your code here
+        # Input 28 x 28
+        self.latent_dim_size = latent_dim_size
+        self.hidden_dim_size = hidden_dim_size
+
+        self.encoder = nn.Sequential(
+            # Conv kernel 4x4, stride 2, padding 1, channels -> 16
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.ReLU(),
+            # Conv kernel 4x4, stride 2, padding 1, channels 16 -> 32
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.ReLU(),
+            # reshape to linear, then Linear layer with hidden_dim_size
+            einops.layers.torch.Rearrange("b c h w -> b (c h w)"),
+            nn.Linear(32 * 7 * 7, hidden_dim_size),
+            nn.ReLU(),
+            # Linear layer with latent_dim_size:
+            nn.Linear(hidden_dim_size, latent_dim_size * 2),
+        )
+        # END OF ENCODER
+        self.decoder = nn.Sequential(
+            # Linear layer with hidden_dim_size
+            nn.Linear(latent_dim_size, hidden_dim_size),
+            nn.ReLU(),
+            # Linear layer with 32*7*7
+            nn.Linear(hidden_dim_size, 32 * 7 * 7),
+            nn.ReLU(),
+            # reshape to 32x7x7
+            einops.layers.torch.Rearrange("b (c h w) -> b c h w", c=32, h=7, w=7),
+            # ConvTranspose kernel 4x4, stride 2, padding 1, channels 32 -> 16
+            nn.ConvTranspose2d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.ReLU(),
+            # ConvTranspose kernel 4x4, stride 2, padding 1, channels 16 -> 1
+            nn.ConvTranspose2d(
+                in_channels=16,
+                out_channels=1,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+        )
+
+    def sample_latent_vector(self, x: t.Tensor) -> t.Tensor:
+        latent_activation = self.encoder(x)
+        mean = latent_activation[-1, :self.latent_dim_size]
+        std = latent_activation[-1, self.latent_dim_size:]
+
+        z = mean + std * torch.randn(latent_activation.shape)
+        return z
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        # Your code here
+        latent = self.sample_latent_vector(x)
+        decoded = self.decoder(latent)
+        
+        return decoded
