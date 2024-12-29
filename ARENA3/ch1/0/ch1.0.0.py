790a4a86ad79b5e0e4eb46964a244e982b75dc5f
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-12-29 22:29:58 +0100

    more boilerplate from ex

--- ARENA3/ch1/0/ch1.0.0.py
+++ ARENA3/ch1/0/ch1.0.0.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
 
+from aiohttp import DataQueue
 import circuitsvis as cv
 import datasets
 import einops
@@ -85,12 +86,12 @@ print(reference_gpt2.to_str_tokens(tokens))
 # %%
 print("Prediction of logits: ")
 logits, cache = reference_gpt2.run_with_cache(tokens, device=device)
-print(logits.shape)
+print(logits.shape, logits[:5])
 
 # %%
 print("Probabilities:")
 probs = logits.softmax(dim=-1)
-print(probs.shape)
+print(probs.shape, probs[:5])
 
 # %%
 print("Predictions at each step: ")
@@ -98,3 +99,97 @@ most_likely_next_tokens = reference_gpt2.tokenizer.batch_decode(logits.argmax(di
 
 print(list(zip(reference_gpt2.to_str_tokens(tokens), most_likely_next_tokens)))
 
+
+# %%
+next_token = logits[0, -1].argmax(dim=-1)
+next_char = reference_gpt2.to_string(next_token)
+print(repr(next_char))
+
+print(f"Sequence so far: {reference_gpt2.to_string(tokens)[0]!r}")
+
+for i in range(20):
+    print(f"{tokens.shape[-1]+1}th char = {next_char!r}")
+    # Define new input sequence, by appending the previously generated token
+    tokens = t.cat([tokens, next_token[None, None]], dim=-1)
+    # Pass our new sequence through the model, to get new output
+    logits = reference_gpt2(tokens)
+    # Get the predicted token at the end of our sequence
+    next_token = logits[0, -1].argmax(dim=-1)
+    # Decode and print the result
+    next_char = reference_gpt2.to_string(next_token)
+
+#%%
+batch = 1
+position = 35
+d_model = 768
+n_heads = 12
+n_layers = 12
+d_mlp = 4 * d_model
+d_head = d_model // n_heads
+
+# %%
+for activation_name, activation in cache.items():
+    # Only print for first layer
+    if ".0." in activation_name or "blocks" not in activation_name:
+        print(f"{activation_name:30} {tuple(activation.shape)}")
+        
+# %%
+for name, param in reference_gpt2.named_parameters():
+    # Only print for first layer
+    if ".0." in name or "blocks" not in name:
+        print(f"{name:18} {tuple(param.shape)}")
+# %%
+
+# As a reference - note there's a lot of stuff we don't care about in here, to do with library internals or other architectures
+print(reference_gpt2.cfg)
+# %%
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+
+cfg = Config()
+print(cfg)
+
+# %%
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randn(shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randint(100, 1000, shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+def load_gpt2_test(cls, gpt2_layer, input):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    print("Input shape:", input.shape)
+    output = layer(input)
+    if isinstance(output, tuple): output = output[0]
+    print("Output shape:", output.shape)
+    try: reference_output = gpt2_layer(input)
+    except: reference_output = gpt2_layer(input, input, input)
+    print("Reference output shape:", reference_output.shape, "\n")
+    comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct\n")
+# %%
