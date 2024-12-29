8b7360dc884d7113eb9611032a68319eb97ab27e
Luigi Petrucco <luigi.petrucco@gmail.com>
2024-12-29 13:07:59 +0100

    starting ch1

---
+++ ARENA3/ch1/0/ch1.0.0.py
@@ -0,0 +1,46 @@
+import math
+import os
+import sys
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+
+import circuitsvis as cv
+import datasets
+import einops
+import numpy as np
+import torch as t
+import torch.nn as nn
+import wandb
+from IPython.display import display
+from jaxtyping import Float, Int
+from rich import print as rprint
+from rich.table import Table
+from torch import Tensor
+from torch.utils.data import DataLoader
+from tqdm.notebook import tqdm
+from transformer_lens import HookedTransformer
+from transformer_lens.utils import gelu_new, tokenize_and_concatenate
+from transformers import PreTrainedTokenizerFast
+from transformers.models.gpt2.tokenization_gpt2_fast import GPT2TokenizerFast
+
+# Make sure exercises are in the path
+chapter = r"chapter1_transformer_interp"
+exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
+section_dir = exercises_dir / "part1_transformer_from_scratch"
+if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+
+import solutions as solutions
+import tests as tests
+
+device = t.device('mps' if t.backends.mps.is_available() else 'cuda' if t.cuda.is_available() else 'cpu')
+
+MAIN = __name__ == '__main__'
+
+reference_gpt2 = HookedTransformer.from_pretrained(
+    "gpt2-small",
+    fold_ln=False,
+    center_unembed=False,
+    center_writing_weights=False,
+    device=device
+)
