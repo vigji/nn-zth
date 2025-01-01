01298b1198ad447e0838b108070da2bcedef5c92
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-01-01 10:04:50 +0100

    blacked

--- ARENA3/ch1/0/ch1.0.0.py
+++ ARENA3/ch1/0/ch1.0.0.py
@@ -34,21 +34,26 @@ from transformers.models.gpt2.tokenization_gpt2_fast import GPT2TokenizerFast
 chapter = r"chapter1_transformer_interp"
 exercises_dir = Path(f"{os.getcwd().split(chapter)[0]}/{chapter}/exercises").resolve()
 section_dir = exercises_dir / "part1_transformer_from_scratch"
-if str(exercises_dir) not in sys.path: sys.path.append(str(exercises_dir))
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
 
 import solutions as solutions
 import tests as tests
 
-device = t.device('mps' if t.backends.mps.is_available() else 'cuda' if t.cuda.is_available() else 'cpu')
+device = t.device(
+    "mps"
+    if t.backends.mps.is_available()
+    else "cuda" if t.cuda.is_available() else "cpu"
+)
 
-MAIN = __name__ == '__main__'
+MAIN = __name__ == "__main__"
 
 reference_gpt2 = HookedTransformer.from_pretrained(
     "gpt2-small",
     fold_ln=False,
     center_unembed=False,
     center_writing_weights=False,
-    device=device
+    device=device,
 )
 
 sorted_vocab = sorted(list(reference_gpt2.tokenizer.vocab.items()), key=lambda n: n[1])
@@ -99,7 +104,9 @@ print(probs.shape, probs[:5])
 
 # %%
 print("Predictions at each step: ")
-most_likely_next_tokens = reference_gpt2.tokenizer.batch_decode(logits.argmax(dim=-1)[0])
+most_likely_next_tokens = reference_gpt2.tokenizer.batch_decode(
+    logits.argmax(dim=-1)[0]
+)
 
 print(list(zip(reference_gpt2.to_str_tokens(tokens), most_likely_next_tokens)))
 
@@ -122,7 +129,7 @@ for i in range(20):
     # Decode and print the result
     next_char = reference_gpt2.to_string(next_token)
 
-#%%
+# %%
 batch = 1
 position = 35
 d_model = 768
@@ -134,9 +141,9 @@ d_head = d_model // n_heads
 # %%
 for activation_name, activation in cache.items():
     # Only print for first layer
-    #if ".0." in activation_name or "blocks" not in activation_name:
-        print(f"{activation_name:30} {tuple(activation.shape)}")
-        
+    # if ".0." in activation_name or "blocks" not in activation_name:
+    print(f"{activation_name:30} {tuple(activation.shape)}")
+
 # %%
 param_count = 0
 for name, param in reference_gpt2.named_parameters():
@@ -150,6 +157,8 @@ param_count
 
 # As a reference - note there's a lot of stuff we don't care about in here, to do with library internals or other architectures
 print(reference_gpt2.cfg)
+
+
 # %%
 @dataclass
 class Config:
@@ -168,6 +177,7 @@ class Config:
 cfg = Config()
 print(cfg)
 
+
 # %%
 def rand_float_test(cls, shape):
     cfg = Config(debug=True)
@@ -175,8 +185,10 @@ def rand_float_test(cls, shape):
     random_input = t.randn(shape).to(device)
     print("Input shape:", random_input.shape)
     output = layer(random_input)
-    if isinstance(output, tuple): output = output[0]
+    if isinstance(output, tuple):
+        output = output[0]
     print("Output shape:", output.shape, "\n")
+
 
 def rand_int_test(cls, shape):
     cfg = Config(debug=True)
@@ -184,8 +196,10 @@ def rand_int_test(cls, shape):
     random_input = t.randint(100, 1000, shape).to(device)
     print("Input shape:", random_input.shape)
     output = layer(random_input)
-    if isinstance(output, tuple): output = output[0]
+    if isinstance(output, tuple):
+        output = output[0]
     print("Output shape:", output.shape, "\n")
+
 
 def load_gpt2_test(cls, gpt2_layer, input):
     cfg = Config(debug=True)
@@ -193,18 +207,23 @@ def load_gpt2_test(cls, gpt2_layer, input):
     layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
     print("Input shape:", input.shape)
     output = layer(input)
-    if isinstance(output, tuple): output = output[0]
+    if isinstance(output, tuple):
+        output = output[0]
     print("Output shape:", output.shape)
-    try: reference_output = gpt2_layer(input)
-    except: reference_output = gpt2_layer(input, input, input)
+    try:
+        reference_output = gpt2_layer(input)
+    except:
+        reference_output = gpt2_layer(input, input, input)
     print("Reference output shape:", reference_output.shape, "\n")
     comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
     print(f"{comparison.sum()/comparison.numel():.2%} of the values are correct\n")
+
 
 # %%
 ###################
 # LayerNorm
 ###################
+
 
 class LayerNorm(nn.Module):
     def __init__(self, cfg: Config):
@@ -213,7 +232,9 @@ class LayerNorm(nn.Module):
         self.w = nn.Parameter(t.ones(cfg.d_model))
         self.b = nn.Parameter(t.zeros(cfg.d_model))
 
-    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+    def forward(
+        self, residual: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
         if self.cfg.debug:
             print("residual shape: ", residual.shape)
 
@@ -225,6 +246,7 @@ class LayerNorm(nn.Module):
 
         return scaled
 
+
 rand_float_test(LayerNorm, [2, 4, 768])
 load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
 zero_input = t.zeros_like(cache["resid_post", 11]).to(device)
@@ -234,13 +256,14 @@ load_gpt2_test(LayerNorm, reference_gpt2.ln_final, zero_input)
 a = t.randint(0, 50, (2, 4))
 W = t.randn(50, 42)
 # einops.einsum(a, W, "b s v, v d -> b s d").shape
-# from einops.layers.torch import 
+# from einops.layers.torch import
 # einops.layers.torch
 W[a, :].shape
 # %%
 ######################
 # Embedding
 ######################
+
 
 class Embed(nn.Module):
     def __init__(self, cfg: Config):
@@ -249,7 +272,9 @@ class Embed(nn.Module):
         self.W_E = nn.Parameter(t.empty((cfg.d_vocab, cfg.d_model)))
         nn.init.normal_(self.W_E, std=self.cfg.init_range)
 
-    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+    def forward(
+        self, tokens: Int[Tensor, "batch position"]
+    ) -> Float[Tensor, "batch position d_model"]:
         if self.cfg.debug:
             print("Input shape: ", tokens.shape)
         return self.W_E[tokens, :]
@@ -262,6 +287,7 @@ load_gpt2_test(Embed, reference_gpt2.embed, tokens)
 # Positional embedding
 #########################
 
+
 class PosEmbed(nn.Module):
     def __init__(self, cfg: Config):
         super().__init__()
@@ -269,7 +295,9 @@ class PosEmbed(nn.Module):
         self.W_pos = nn.Parameter(t.empty((cfg.n_ctx, cfg.d_model)))
         nn.init.normal_(self.W_pos, std=self.cfg.init_range)
 
-    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+    def forward(
+        self, tokens: Int[Tensor, "batch position"]
+    ) -> Float[Tensor, "batch position d_model"]:
         if self.cfg.debug:
             print("Input shape: ", tokens.shape)
 
@@ -305,107 +333,133 @@ class Attention(nn.Module):
         nn.init.normal_(self.W_K, std=self.cfg.init_range)
         nn.init.normal_(self.W_V, std=self.cfg.init_range)
         nn.init.normal_(self.W_O, std=self.cfg.init_range)
-        self.register_buffer("IGNORE", t.tensor(float("-inf"), 
-                                                dtype=t.float32, 
-                                                device=device))
-        
-    def forward(self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        self.register_buffer(
+            "IGNORE", t.tensor(float("-inf"), dtype=t.float32, device=device)
+        )
+
+    def forward(
+        self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
         # Calculate query, key and value vectors
         q = (
             einops.einsum(
-                normalized_resid_pre, self.W_Q, "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head"
+                normalized_resid_pre,
+                self.W_Q,
+                "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head",
             )
             + self.b_Q
         )
         k = (
             einops.einsum(
-                normalized_resid_pre, self.W_K, "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head"
+                normalized_resid_pre,
+                self.W_K,
+                "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head",
             )
             + self.b_K
         )
         v = (
             einops.einsum(
-                normalized_resid_pre, self.W_V, "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head"
+                normalized_resid_pre,
+                self.W_V,
+                "batch posn d_model, nheads d_model d_head -> batch posn nheads d_head",
             )
             + self.b_V
         )
-        # print(q[0, 0, :2, :2])
-        # print(k[0, 0, :2, :2])
-        # print(v[0, 0, :2, :2])
 
         # Calculate attention scores, then scale and mask, and apply softmax to get probabilities
         attn_scores = einops.einsum(
-            q, k, "batch posn_Q nheads d_head, batch posn_K nheads d_head -> batch nheads posn_Q posn_K"
+            q,
+            k,
+            "batch posn_Q nheads d_head, batch posn_K nheads d_head -> batch nheads posn_Q posn_K",
         )
         attn_scores_masked = self.apply_causal_mask(attn_scores / self.cfg.d_head**0.5)
         attn_pattern = attn_scores_masked.softmax(-1)
-        # print(attn_pattern[0, 0, :3, :3])
 
         # Take weighted sum of value vectors, according to attention probabilities
-        # print(v[0, 0, :3, :3])
-        # print(attn_pattern[0, 0, :3, :3])
         z = einops.einsum(
-            v, attn_pattern, "batch posn_K nheads d_head, batch nheads posn_Q posn_K -> batch posn_Q nheads d_head"
+            v,
+            attn_pattern,
+            "batch posn_K nheads d_head, batch nheads posn_Q posn_K -> batch posn_Q nheads d_head",
         )
         print(z[0, 0, :3, :3])
 
         # Calculate output (by applying matrix W_O and summing over heads, then adding bias b_O)
         attn_out = (
-            einops.einsum(z, self.W_O, "batch posn_Q nheads d_head, nheads d_head d_model -> batch posn_Q d_model")
+            einops.einsum(
+                z,
+                self.W_O,
+                "batch posn_Q nheads d_head, nheads d_head d_model -> batch posn_Q d_model",
+            )
             + self.b_O
         )
 
         return attn_out
 
-    def forward_mine(self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+    def forward_mine(
+        self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
         # Compute Q, K and V activations:
-        q_activations = einops.einsum(normalized_resid_pre, self.W_Q, 
-                                 "batch posn d_model, n_heads d_model d_head -> batch posn n_heads d_head") + self.b_Q
-        k_activations = einops.einsum(normalized_resid_pre, self.W_K, 
-                                 "batch posn d_model, n_heads d_model d_head -> batch posn n_heads d_head") + self.b_K
-        v_activations = einops.einsum(normalized_resid_pre, self.W_V, 
-                                 "batch posn d_model, n_heads d_model d_head -> batch posn n_heads d_head") + self.b_V
-        # print(q_activations[0, 0, :2, :2])
-        # print(k_activations[0, 0, :2, :2])
-        # print(v_activations[0, 0, :2, :2])
+        q_activations = (
+            einops.einsum(
+                normalized_resid_pre,
+                self.W_Q,
+                "batch posn d_model, n_heads d_model d_head -> batch posn n_heads d_head",
+            )
+            + self.b_Q
+        )
+        k_activations = (
+            einops.einsum(
+                normalized_resid_pre,
+                self.W_K,
+                "batch posn d_model, n_heads d_model d_head -> batch posn n_heads d_head",
+            )
+            + self.b_K
+        )
+        v_activations = (
+            einops.einsum(
+                normalized_resid_pre,
+                self.W_V,
+                "batch posn d_model, n_heads d_model d_head -> batch posn n_heads d_head",
+            )
+            + self.b_V
+        )
 
         # Get attention matrix:
-        attention_logits = einops.einsum(q_activations, k_activations, 
-                                 "batch posq n_heads d_head, batch posk n_heads d_head -> batch n_heads posq posk")
+        attention_logits = einops.einsum(
+            q_activations,
+            k_activations,
+            "batch posq n_heads d_head, batch posk n_heads d_head -> batch n_heads posq posk",
+        )
 
         normed_masked_attention_logits = self.apply_causal_mask(
-            attention_logits / t.sqrt(t.tensor(cfg.d_head, dtype=t.float32, device=device)))
+            attention_logits
+            / t.sqrt(t.tensor(cfg.d_head, dtype=t.float32, device=device))
+        )
         attention_p = t.softmax(normed_masked_attention_logits, dim=3)
-        # print(attention_p[0, 0, :3, :3])
-
-        # from sol:
-        # attn_scores_masked = self.apply_causal_mask(attention_logits / self.cfg.d_head**0.5)
-        # attention_p = attn_scores_masked.softmax(-1)
-
-        # Apply and project to output:
-        # print(v_activations[0, 0, :3, :3])
-        # print(attention_p[0, 0, :3, :3])
 
         z = einops.einsum(
-            v_activations, attention_p,
-            "batch posk n_heads d_head, batch n_heads posq posk -> batch posq n_heads d_head")
-        print(z[0, 0, :3, :3])
-        
-        result = einops.einsum(z, self.W_O, 
-                               "batch posq n_heads d_head, n_heads d_head d_model -> batch posq d_model") + self.b_O
+            v_activations,
+            attention_p,
+            "batch posk n_heads d_head, batch n_heads posq posk -> batch posq n_heads d_head",
+        )
 
-        # sum_over_heads = einops.einsum(result,
-        #                       "batch posq n_heads d_model -> batch posq d_model")
-        
-        return result 
-        
+        result = (
+            einops.einsum(
+                z,
+                self.W_O,
+                "batch posq n_heads d_head, n_heads d_head d_model -> batch posq d_model",
+            )
+            + self.b_O
+        )
+
+        return result
 
     def apply_causal_mask(
         self, attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"]
     ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
-        '''
+        """
         Applies a causal mask to attention scores, and returns masked scores.
-        '''
+        """
         _, _, n_q, n_k = attn_scores.shape
 
         all_ones = t.ones(n_q, n_k, device=device, dtype=bool)
@@ -415,4 +469,51 @@ class Attention(nn.Module):
         return t.masked_fill(attn_scores, mask, self.IGNORE)
 
 
+# %%
+# Implementing MLP:
+
+
+class MLP(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_in = nn.Parameter(t.empty((cfg.d_model, cfg.d_mlp)))
+        self.W_out = nn.Parameter(t.empty((cfg.d_mlp, cfg.d_model)))
+        self.b_in = nn.Parameter(t.zeros((cfg.d_mlp)))
+        self.b_out = nn.Parameter(t.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_in, std=self.cfg.init_range)
+        nn.init.normal_(self.W_out, std=self.cfg.init_range)
+
+    def forward(
+        self, normalized_resid_mid: Float[Tensor, "batch posn d_model"]
+    ) -> Float[Tensor, "batch posn d_model"]:
+        h_input = (
+            einops.einsum(
+                normalized_resid_mid,
+                self.W_in,
+                "batch posn d_model, d_model d_mlp -> batch posn d_mlp",
+            )
+            + self.b_in
+        )
+
+        h_activation = gelu_new(h_input)
+
+        return (
+            einops.einsum(
+                h_activation,
+                self.W_out,
+                "batch posn d_mlp, d_mlp d_model -> batch posn d_model",
+            )
+            + self.b_out
+        )
+
+
+rand_float_test(MLP, [2, 4, 768])
+load_gpt2_test(MLP, reference_gpt2.blocks[0].mlp, cache["normalized", 0, "ln2"])
+# %%
+einops.einsum(
+    t.ones(2, 4, 768),
+    t.ones(768),
+    "batch posn d_model, d_mlp d_model -> batch posn d_mlp",
+)
 # %%
