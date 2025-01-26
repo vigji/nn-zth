36d8e66232138ac7f51bfdb3a9d56748ac6a2dbe
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-01-26 12:31:21 +0100

    induction head detector hook

--- ARENA3/ch1/1/ch1_1_2.py
+++ ARENA3/ch1/1/ch1_1_2.py
@@ -150,3 +150,44 @@ loss = model.run_with_hooks(
     ]
 )
 # %%
+seq_len = 50
+batch_size = 10
+rep_tokens_10 = generate_repeated_tokens(model, seq_len, batch_size)
+
+# We make a tensor to store the induction score for each head.
+# We put it on the model's device to avoid needing to move things between the GPU and CPU, which can be slow.
+induction_score_store = t.zeros((model.cfg.n_layers, model.cfg.n_heads), device=model.cfg.device)
+
+
+def induction_score_hook(pattern: Float[Tensor, "batch head_index dest_pos source_pos"], hook: HookPoint):
+    """
+    Calculates the induction score, and stores it in the [layer, head] position of the `induction_score_store` tensor.
+    """
+    i_layer = int(hook.name.split(".")[1])
+    seq_length = (pattern.shape[-1] - 1) // 2
+    scores = pattern.diagonal(-(seq_length-1), dim1=2, dim2=3).mean((0, 2))
+    induction_score_store[i_layer, :] = scores
+
+    return pattern
+
+
+# We make a boolean filter on activation names, that's true only on attention pattern names
+pattern_hook_names_filter = lambda name: name.endswith("pattern")
+
+# Run with hooks (this is where we write to the `induction_score_store` tensor`)
+model.run_with_hooks(
+    rep_tokens_10,
+    return_type=None,  # For efficiency, we don't need to calculate the logits
+    fwd_hooks=[(pattern_hook_names_filter, induction_score_hook)],
+)
+
+# Plot the induction scores for each head in each layer
+imshow(
+    induction_score_store,
+    labels={"x": "Head", "y": "Layer"},
+    title="Induction Score by Head",
+    text_auto=".2f",
+    width=900,
+    height=350,
+)
+# %%
