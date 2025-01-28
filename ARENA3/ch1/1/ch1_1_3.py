111c381ac0b2a096ad8dc630a75d2f17ac86fb0d
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-01-28 23:08:38 +0100

    started 1.1.3

---
+++ ARENA3/ch1/1/ch1_1_3.py
@@ -0,0 +1,87 @@
+# %%
+import functools
+import sys
+from pathlib import Path
+import token
+from typing import Callable
+
+import circuitsvis as cv
+import einops
+import numpy as np
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as F
+from IPython.display import display
+from jaxtyping import Float, Int
+from torch import Tensor
+from tqdm import tqdm
+from transformer_lens import (
+    ActivationCache,
+    FactoredMatrix,
+    HookedTransformer,
+    HookedTransformerConfig,
+    utils,
+)
+from transformer_lens.hook_points import HookPoint
+
+device = t.device(
+    "mps"
+    if t.backends.mps.is_available()
+    else "cuda" if t.cuda.is_available() else "cpu"
+)
+
+# Make sure exercises are in the path
+chapter = "chapter1_transformer_interp"
+section = "part2_intro_to_mech_interp"
+
+import tests
+from plotly_utils import (
+    hist,
+    imshow,
+    plot_comp_scores,
+    plot_logit_attribution,
+    plot_loss_difference,
+)
+
+# Saves computation time, since we don't need it for the contents of this notebook
+t.set_grad_enabled(False)
+
+MAIN = __name__ == "__main__"
+# %%
+
+A = t.randn(5, 2)
+B = t.randn(2, 5)
+AB = A @ B
+AB_factor = FactoredMatrix(A, B)
+print("Norms:")
+print(AB.norm())
+print(AB_factor.norm())
+
+print(f"Right dimension: {AB_factor.rdim}, Left dimension: {AB_factor.ldim}, Hidden dimension: {AB_factor.mdim}")
+# %%
+
+print("Eigenvalues:")
+print(t.linalg.eig(AB).eigenvalues)
+print(AB_factor.eigenvalues)
+
+print("\nSingular Values:")
+print(t.linalg.svd(AB).S)
+print(AB_factor.S)
+
+print("\nFull SVD:")
+print(AB_factor.svd())
+# %%
+
+C = t.randn(5, 300)
+ABC = AB @ C
+ABC_factor = AB_factor @ C
+
+print(f"Unfactored: shape={ABC.shape}, norm={ABC.norm()}")
+print(f"Factored: shape={ABC_factor.shape}, norm={ABC_factor.norm()}")
+print(f"\nRight dimension: {ABC_factor.rdim}, "
+      f"Left dimension: {ABC_factor.ldim}, "
+      f"Hidden dimension: {ABC_factor.mdim}")
+
+AB_unfactored = AB_factor.AB
+t.testing.assert_close(AB_unfactored, AB)
+# %%
