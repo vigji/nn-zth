45f6cb641e884186e58ffce8e36e12b7c609a9dd
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-02-03 23:08:05 +0100

    blacked

--- ARENA3/ch1/1/ch1_1_3.py
+++ ARENA3/ch1/1/ch1_1_3.py
@@ -27,7 +27,9 @@ from transformer_lens.hook_points import HookPoint
 device = t.device(
     "mps"
     if t.backends.mps.is_available()
-    else "cuda" if t.cuda.is_available() else "cpu"
+    else "cuda"
+    if t.cuda.is_available()
+    else "cpu"
 )
 
 # Make sure exercises are in the path
