f58caae6c93a637483907a95a38d1bafdb588e0b
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-07-21 07:52:41 +0200

    car example

--- sutton_barto/ch4_dynamic_programming/cars_example_42.py
+++ sutton_barto/ch4_dynamic_programming/cars_example_42.py
@@ -18,6 +18,9 @@ def poisson(lam, n):
 # Location A: lambda=3 for request, 3 for return
 # Location B: lambda=4 for request, 2 for return
 
+lambdas_dict = {"locA": {"request": 4, "return": 3}, 
+                "locB": {"request": 3, "return": 2}}
+
 # no more than 20 cars per location (more disappear)
 
 # discount rate 0.9
@@ -34,4 +37,29 @@ policy_state = [[0 for _ in range(max_n + 1)] for _ in range(max_n + 1)]
 value_state = [[0 for _ in range(max_n + 1)] for _ in range(max_n + 1)]
 print(value_state)
 
+def trans_prob(state, action, location, max_n):
+    request_lamb, return_lamb = lambdas_dict[location]["request"], lambdas_dict[location]["return"]
 
+    probs_rent = [0 for _ in range(max_n + 1)]
+    probs_returned = [0 for _ in range(max_n + 1)]
+    # car requested:
+    for n in range(state):
+        probs_rent[n] = poisson(request_lamb, n)
+
+    # car returned
+    for n in range(max_n - state):
+        probs_returned[n] = poisson(return_lamb, n)
+
+    # final probabilities:
+    for n in range(max_n + 1):
+
+    return probs_rent, probs_returned
+
+
+state = 10
+arr, arr2 = trans_prob(state, 0, "locA", max_n)
+
+plt.figure()
+plt.plot(arr)
+plt.plot(arr2)
+plt.show()
