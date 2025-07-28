f0e6d16073888c4a7da378ddc7d36677a1ef57e9
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-07-28 07:32:33 +0200

    gymnasium

---
+++ RL_lapan/ch2_gym_random.py
@@ -0,0 +1,53 @@
+import gymnasium as gym
+import random
+
+e = gym.make("CartPole-v1")
+
+obs, info = e.reset()
+print(obs)
+print(info)
+
+print(e.action_space)
+
+print(e.observation_space)
+
+total_reward = 0
+while True:
+    action = e.action_space.sample()
+    obs, r, done, truncated, _ = e.step(action)
+    total_reward += r
+    if done or truncated:
+        break
+
+print("Total reward: ", total_reward)
+
+
+class RandomActionWrapper(gym.ActionWrapper):
+    def __init__(self, env: gym.Env, epsilon: float = 0.1):
+        super().__init__(env)
+        self.epsilon = epsilon
+
+    def action(self, action: gym.core.WrapperActType) -> gym.core.WrapperActType:
+
+        if random.random() < self.epsilon:
+            action = self.env.action_space.sample()
+            print(f"Random action {action}")
+
+        return action
+    
+
+if __name__ == "__main__":
+    env = RandomActionWrapper(gym.make("CartPole-v1", render_mode="rgb_array"))
+    env = gym.wrappers.RecordVideo(env, video_folder="/Users/vigji/Desktop")
+
+    obs = env.reset()
+    total_reward = 0.
+
+    while True:
+        obs, r, done, truncated, _ = env.step(0)
+        total_reward += r
+        if done or truncated:
+            break
+
+    print("Total reward: ", total_reward)
+    env.close()
--- RL_lapan/ch2_random.py
+++ RL_lapan/ch2_random.py
@@ -14,4 +14,34 @@ class Environment:
     def is_done(self):
         return self.steps_left == 0
     
+    def action(self, action: int) -> float:
+        if self.is_done():
+            raise Exception("Game ended!")
+        
+        self.steps_left -= 1
+        return random.random() # in this example only random
     
+
+class Agent:
+    def __init__(self):
+        self.total_reward = 0.0
+
+    def step(self, env: Environment):
+        observations = env.get_observation()
+        action = env.get_actions()
+
+        # here, random for the example:
+        choosen_action = random.choice(action)
+        reward = env.action(choosen_action)
+        self.total_reward += reward
+
+
+if __name__ == "__main__":
+
+    env = Environment()
+    agent = Agent()
+
+    while not env.is_done():
+        agent.step(env)
+
+    print(f"Total: {agent.total_reward:.4f}")
