b93b31452e3fc78cc63aea39f4f590de65b22c95
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-07-31 23:53:58 +0200

    v-learning

--- RL_lapan/ch05_q_learning/01_frozenlake_v_iteration.py
+++ RL_lapan/ch05_q_learning/01_frozenlake_v_iteration.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
 import typing as tt
 import gymnasium as gym
 from collections import defaultdict, Counter
 from torch.utils.tensorboard.writer import SummaryWriter
-from pprint import pprint
-
 
 ENV_NAME = "FrozenLake-v1"
+#ENV_NAME = "FrozenLake8x8-v1"      # uncomment for larger version
 GAMMA = 0.9
 TEST_EPISODES = 20
 
@@ -17,25 +17,21 @@ TransitKey = tt.Tuple[State, Action]
 
 class Agent:
     def __init__(self):
-        self.env: gym.Env = gym.make(ENV_NAME)
-
+        self.env = gym.make(ENV_NAME)
         self.state, _ = self.env.reset()
         self.rewards: tt.Dict[RewardKey, float] = defaultdict(float)
         self.transits: tt.Dict[TransitKey, Counter] = defaultdict(Counter)
-
         self.values: tt.Dict[State, float] = defaultdict(float)
 
-    
-    def play_n_random(self, n : int):
+    def play_n_random_steps(self, n: int):
         for _ in range(n):
             action = self.env.action_space.sample()
-
-            new_state, reward, term_flag, trunc_flag, _ = self.env.step(action)
+            new_state, reward, is_done, is_trunc, _ = self.env.step(action)
             rw_key = (self.state, action, new_state)
             self.rewards[rw_key] = float(reward)
-            trans_key = (self.state, action)
-            self.transits[trans_key][new_state] += 1
-            if term_flag or trunc_flag:
+            tr_key = (self.state, action)
+            self.transits[tr_key][new_state] += 1
+            if is_done or is_trunc:
                 self.state, _ = self.env.reset()
             else:
                 self.state = new_state
@@ -50,7 +46,7 @@ class Agent:
             val = reward + GAMMA * self.values[tgt_state]
             action_value += (count / total) * val
         return action_value
-    
+
     def select_action(self, state: State) -> Action:
         best_action, best_value = None, None
         for action in range(self.env.action_space.n):
@@ -59,71 +55,53 @@ class Agent:
                 best_value = action_value
                 best_action = action
         return best_action
-                
-    def play_episode(self, env: gym.Env):
+
+    def play_episode(self, env: gym.Env) -> float:
         total_reward = 0.0
         state, _ = env.reset()
         while True:
             action = self.select_action(state)
-            new_state, reward, end_flag, trunc_flag, _ = env.step(action)
-            total_reward += reward
+            new_state, reward, is_done, is_trunc, _ = env.step(action)
             rw_key = (state, action, new_state)
             self.rewards[rw_key] = float(reward)
-            trans_key = (state, action)
-            self.transits[trans_key][new_state] += 1
-            if end_flag or trunc_flag:
+            tr_key = (state, action)
+            self.transits[tr_key][new_state] += 1
+            total_reward += reward
+            if is_done or is_trunc:
                 break
             state = new_state
-        #print(rew_list)
-
         return total_reward
-    
+
     def value_iteration(self):
         for state in range(self.env.observation_space.n):
-            possible_actions = range(self.env.action_space.n)
-            values_after_action = [self.calc_action_value(state, action) for action in possible_actions]
-
-            self.values[state] = max(values_after_action)
-
-
-        #pprint(self.values)
-
-        
-        
+            state_values = [
+                self.calc_action_value(state, action)
+                for action in range(self.env.action_space.n)
+            ]
+            self.values[state] = max(state_values)
 
 
 if __name__ == "__main__":
-    from pprint import pprint
+    test_env = gym.make(ENV_NAME)
     agent = Agent()
     writer = SummaryWriter(comment="-v-iteration")
-    test_env = gym.make(ENV_NAME)
 
-    best_reward = 0.
-    i_iter = 0
+    iter_no = 0
+    best_reward = 0.0
     while True:
-        # print(i_iter)
-        agent.play_n_random(100)
+        iter_no += 1
+        agent.play_n_random_steps(100)
         agent.value_iteration()
-        if i_iter > 100:
-            break
 
-        # test part
-        total_reward = 0.
+        reward = 0.0
         for _ in range(TEST_EPISODES):
-            total_reward += agent.play_episode(env=test_env)
-        total_reward /= TEST_EPISODES
-        #print(total_reward)
-
-        if total_reward > best_reward:
-            print(f"{i_iter}: Best reward updated {best_reward:.3} -> {total_reward:.3}")
-            best_reward = total_reward
-
-        writer.add_scalar("test", total_reward, i_iter)
-
-        if total_reward > 0.8:
-            print(f"Solved: reward {total_reward} after {i_iter} iterations")
+            reward += agent.play_episode(test_env)
+        reward /= TEST_EPISODES
+        writer.add_scalar("reward", reward, iter_no)
+        if reward > best_reward:
+            print(f"{iter_no}: Best reward updated {best_reward:.3} -> {reward:.3}")
+            best_reward = reward
+        if reward > 0.80:
+            print("Solved in %d iterations!" % iter_no)
             break
-
-        i_iter += 1
-
     writer.close()
